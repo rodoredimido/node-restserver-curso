d185ad3628fa304e2281ad13dbc4f138b14fbf7d
rodoredimido <rodolfo_lacruz_101@hotmail.com>
2018-12-25 21:18:26 -0200

    vARIABLE DE ENTORNO MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds143604.mlab.com:43604/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
